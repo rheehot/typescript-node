57f30abc06cf68dfc7c2ae70a930ad4df5cea8b9
Larry Jung <jhj46456@gmail.com>
2020-04-11 15:18:31 +0900

    build

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # typescript-node
+
 ts로 express 서버 만들어보기
+
+- ORM : 2.0.0-beta.2
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as express from "express";
+import * as logger from "morgan";
 import apiRouter from "./routers/apiRouter";
 
 const app = express();
+
+app.use(logger(process?.env?.NODE_ENV === "production" ? "combined" : "dev"));
 
 app.use("/api", apiRouter);
 
